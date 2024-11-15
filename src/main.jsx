259537a88d4863ef3314645c12c55c97ba35916a
anpapag1 <anpapag1@cs.ihu.gr>
2024-11-15 21:16:34 +0200

    settings title and icon set

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Sidebar />
-    <Settings />
+    <Settings value='Button configurator' />
   </React.StrictMode>,
 );
--- src/settings.jsx
+++ src/settings.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './settings.css';
+import { TbKeyboard } from "react-icons/tb";
 
 
-function Settings () {
+function Settings(text) {
+    const [open, setShow] = useState(true);
+
     return (
-        <div className='settings'>
+        <div 
+            className={`settings ${open ? 'open' : ''}`} 
+            onMouseEnter={() => setShow(true)} 
+            // onMouseLeave={() => setShow(false)}
+        >
+            <div className='settingsHeader'>
+                <div className='settingsIcon'>
+                    <TbKeyboard />
+                </div>
+                <div className='settingsTitle'>{text.value}</div>
+            </div>
             <div className='border'>
                 
             </div>
